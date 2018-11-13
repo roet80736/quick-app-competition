5f90957acfd6e5f1008387fa2dc92f3bc1665afd
西祠梧桐 <919964333@qq.com>
2018-11-13 23:45:48 +0800

    init

---
+++ travel-notes/src/config.js
@@ -0,0 +1,9 @@
+const config = {
+    // apiUrl: 'https://www.xiciwutong.xin/travel-notes/', // 接口的地址
+  apiUrl: 'http://192.168.0.3:9345/travel-notes/',
+
+  imageUrl: 'https://www.xiciwutong.xin'
+}
+export default {
+    config
+}
